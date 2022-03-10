46bf8675abf53f344fe20bbd30b0fb42bda96fdd
jcguerra10 <juancamiloguerratabares@gmail.com>
2022-03-10 17:09:13 -0500

    ChangeOnFragments

--- app/src/main/java/com/reto1/model/User.kt
+++ app/src/main/java/com/reto1/model/User.kt
@@ -1,11 +1,15 @@
 package com.reto1.model
 
+import java.io.File
+
 class User {
+    var name: String
     var mail: String
     var pass: String
 
     constructor(mail: String, pass: String) {
         this.mail = mail
         this.pass = pass
+        this.name = "DefaultName"
     }
 }
